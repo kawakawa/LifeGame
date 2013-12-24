aead7fbd31fe31966536c862a2f115d443a87ec7
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2013-12-24 17:25:25 +0900

    BoardChangedイベントの作成

---
+++ LifeGame/LifeGame/Model/BoardChangedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeGame.Model
+{
+    /// <summary>
+    /// Changeイベントで使われる EventArgs
+    /// </summary>
+    public class BoardChangedEventArgs:EventArgs
+    {
+        public Location Location { get; internal set; }
+        public IPiece Piece { get; internal set; }
+    }
+}
