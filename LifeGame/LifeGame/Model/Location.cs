e8ed9376b3707f42c48f41ff5933082fdcb2da9c
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2013-12-24 16:25:47 +0900

    Locationクラスの追加

---
+++ LifeGame/LifeGame/Model/Location.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace LifeGame.Model
+{
+    /// <summary>
+    /// ボード上の位置を示す
+    /// </summary>
+    public class Location
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Location(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1}) ", X, Y);
+        }
+    }
+}
