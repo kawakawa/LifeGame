fb6fb92b8a34b124b35be0c3f3d1c8744e9fb906
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2013-12-24 13:53:06 +0900

    Ipiece　インターフェース追加

---
+++ LifeGame/LifeGame/Model/IPiece.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeGame.Model
+{
+    /// <summary>
+    /// 駒を示すマーカーインターフェース(marker interface)
+    /// </summary>
+    public interface IPiece
+    {
+    }
+}
