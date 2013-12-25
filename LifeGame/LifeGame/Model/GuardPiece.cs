d13ac968d30e5ccbf4da6e7266e73d76dc675f85
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2013-12-25 18:50:05 +0900

    マーカーオブジェクトの追加

---
+++ LifeGame/LifeGame/Model/GuardPiece.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeGame.Model
+{
+    // 番兵用：他と区別するためだけなので中身は何でも良い (マーカーオブジェクト)
+    public struct GuardPiece:IPiece
+    {
+    }
+}
