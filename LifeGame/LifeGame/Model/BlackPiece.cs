0d6bd34665ae6a925a080e39b1979c466330607d
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2013-12-25 18:46:28 +0900

    黒石、白石情報の追加

---
+++ LifeGame/LifeGame/Model/BlackPiece.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace LifeGame.Model
+{
+    public struct BlackPiece:IColorPiece
+    {
+        public Color Color
+        {
+            get { return Color.FromArgb(255, 128, 128, 128); }
+        }
+    }
+}
