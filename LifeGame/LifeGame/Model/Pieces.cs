4419acaa17a9d142378a0e07817822fe39e2325f
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2013-12-25 18:50:54 +0900

    Piecesの追加

---
+++ LifeGame/LifeGame/Model/Pieces.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeGame.Model
+{
+    /// <summary>
+    /// 良く利用する駒たち
+    /// </summary>
+    public static class Pieces
+    {
+        public static readonly IPiece Black = new BlackPiece();
+        public static readonly IPiece White = new WhitePiece();
+        public static readonly IPiece Empty = new EmptyPiece();
+        public static readonly IPiece Guard = new GuardPiece();
+
+    }
+}
