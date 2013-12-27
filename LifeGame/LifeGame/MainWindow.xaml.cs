df2bd484063c7c2c03a2516eedc9b461bc7ede76
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2013-12-27 10:24:41 +0900

    canvas1_Loaded method add

--- LifeGame/LifeGame/MainWindow.xaml.cs
+++ LifeGame/LifeGame/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using LifeGame.Model;
 
 namespace LifeGame
 {
@@ -20,9 +22,33 @@ namespace LifeGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
+        private LifeBoard board;
+        private Drawer drawer;
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Canvasがロードされた。初期化をする。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void canvas1_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick+=new EventHandler(timer_Tick);
+
+            board=new LifeBoard(30);
+            
+            drawer=new Drawer(Canvas1,board);
+            drawer.DrawRuledLines(BoardType.Chess);
         }
 
         private void buttonStart_Click(object sender, RoutedEventArgs e)
@@ -45,10 +71,6 @@ namespace LifeGame
             throw new NotImplementedException();
         }
 
-        private void canvas1_Loaded(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
 
         private void canvas1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -62,7 +84,10 @@ namespace LifeGame
 
         private void canvas1_MouseMove(object sender, MouseEventArgs e)
         {
-            throw new NotImplementedException();
+            
+
+
+
         }
     }
 }
