428e935ce5d8142430f33a14c2bba7f086baa2ff
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2013-12-27 10:07:33 +0900

    Drawer class add

---
+++ LifeGame/LifeGame/Model/Drawer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LifeGame.Model
+{
+    /// <summary>
+    /// 描画を受け持つクラス
+    /// </summary>
+    public class Drawer:BoardCanvas
+    {
+        public Drawer(Panel panel1, Board board)
+            : base(panel1, board) { }
+
+
+
+    }
+}
