f64ea29298c1e50591dc7886e167006767b6cd5a
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2013-12-27 10:12:26 +0900

    DrawPiece method add

--- LifeGame/LifeGame/Model/Drawer.cs
+++ LifeGame/LifeGame/Model/Drawer.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace LifeGame.Model
 {
@@ -16,6 +17,22 @@ namespace LifeGame.Model
             : base(panel1, board) { }
 
 
-
+        /// <summary>
+        /// ひとつのセルをその状態によって描画
+        /// </summary>
+        /// <param name="loc"></param>
+        /// <param name="piece"></param>
+        public override　void DrawPiece(Location loc, IPiece piece)
+        {
+            var cell = piece as Cell;
+            if (cell.IsAlive == true)
+            {
+                DrawLife(loc, Colors.Gray);
+            }
+            else
+            {
+                RemovePiece(loc);
+            }
+        }
     }
 }
